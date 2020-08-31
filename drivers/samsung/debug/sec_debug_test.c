e9927d3ee886c2df1c8b419ae3516b344a70f611
Ian Macdonald <ian@caliban.org>
2020-08-31 18:10:33 +0200

    Fix relocation truncation errors when CONFIG_RKP_CFP_JOPP is unset.
    drivers/soc/samsung/debug/exynos-debug-test.o: In function `simulate_WRITE_RO':
    /home/ianmacd/src/c2s/drivers/soc/samsung/debug/exynos-debug-test.c:797:(.text+0x1804): relocation truncated to fit: R_AARCH64_LDST64_ABS_LO12_NC against `.text'
    /home/ianmacd/src/c2s/drivers/soc/samsung/debug/exynos-debug-test.c:797: warning: One possible cause of this error is that the symbol is being referenced in the indicated code as if it had a larger alignment than was declared where it was defined.

--- drivers/samsung/debug/sec_debug_test.c
+++ drivers/samsung/debug/sec_debug_test.c
@@ -749,7 +749,7 @@ static void simulate_UNALIGNED(char *arg)
 {
 	static u8 data[5] __aligned(4) = {1, 2, 3, 4, 5};
 	u32 *p;
-	u32 val = 0x12345678;
+	u32 val = 0x0;
 
 	pr_crit("%s()\n", __func__);
 
@@ -770,7 +770,7 @@ static void simulate_WRITE_RO(char *arg)
 #else
 	ptr = (unsigned long *)simulate_WRITE_RO;
 #endif
-	*ptr ^= 0x12345678;
+	*ptr ^= 0x0;
 }
 
 #define BUFFER_SIZE SZ_1K
