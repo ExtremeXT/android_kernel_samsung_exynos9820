e71eee51512983ee9907b83e226ed7d288b36388
Ocin4ever <winicoco24@gmail.com>
2024-06-08 20:22:46 +0200

    drivers/gpu/arm: backport R38P1 driver from M625F

--- drivers/gpu/arm/bv_r38p1/csf/mali_kbase_csf_kcpu_debugfs.h
+++ drivers/gpu/arm/bv_r38p1/csf/mali_kbase_csf_kcpu_debugfs.h
@@ -1,12 +1,12 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 /*
- * (C) COPYRIGHT 2021 Samsung Electronics Inc. All rights reserved.
+ *
+ * (C) COPYRIGHT 2019-2021 ARM Limited. All rights reserved.
  *
  * This program is free software and is provided to you under the terms of the
  * GNU General Public License version 2 as published by the Free Software
  * Foundation, and any use by you of this program is subject to the terms
- * of such GNU licence.
+ * of such GNU license.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,16 +16,22 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, you can access it online at
  * http://www.gnu.org/licenses/gpl-2.0.html.
+ *
  */
 
-#ifndef _GPEX_COMMON_H_
-#define _GPEX_COMMON_H_
+#ifndef _KBASE_CSF_KCPU_DEBUGFS_H_
+#define _KBASE_CSF_KCPU_DEBUGFS_H_
 
-#include <linux/types.h>
+/* Forward declaration */
+struct kbase_context;
 
-struct platform_context {
-	int cmar_boost;
-	pid_t pid;
-};
+#define MALI_CSF_KCPU_DEBUGFS_VERSION 0
 
-#endif /* _GPEX_COMMON_H_ */
+/**
+ * kbase_csf_kcpu_debugfs_init() - Create a debugfs entry for KCPU queues
+ *
+ * @kctx: The kbase_context for which to create the debugfs entry
+ */
+void kbase_csf_kcpu_debugfs_init(struct kbase_context *kctx);
+
+#endif /* _KBASE_CSF_KCPU_DEBUGFS_H_ */
