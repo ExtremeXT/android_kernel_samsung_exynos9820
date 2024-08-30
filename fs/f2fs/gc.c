887968ab79d9e43fd39d0028dd582a30f84c8620
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2024-08-30 16:18:46 +0300

    f2fs: fix radix

--- fs/f2fs/gc.c
+++ fs/f2fs/gc.c
@@ -1222,7 +1222,7 @@ int f2fs_gc(struct f2fs_sb_info *sbi, bool sync,
 	unsigned int init_segno = segno;
 	struct gc_inode_list gc_list = {
 		.ilist = LIST_HEAD_INIT(gc_list.ilist),
-		.iroot = RADIX_TREE_INIT(GFP_NOFS),
+		.iroot = RADIX_TREE_INIT(gc_list.iroot, GFP_NOFS),
 	};
 	unsigned long long last_skipped = sbi->skipped_atomic_files[FG_GC];
 	unsigned long long first_skipped, gc_start_time = 0, gc_end_time = 0;
